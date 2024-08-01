7e735ed117ddcd8de81a8708b09f4a3fef2b4de9
Mahmoud S Albohaisi <mohabohaisi@gmail.com>
2024-08-01 10:49:56 +0300

    add gsap animations

--- js/main.js
+++ js/main.js
@@ -11,3 +11,15 @@ openBarBtn.addEventListener("click", function () {
 closeBarBtn.addEventListener("click", function () {
   listLink.classList.remove("active");
 });
+
+// GSAP CODE
+
+const myText = new SplitType("#reveal");
+console.log(myText);
+gsap.to('.char', {
+  y: 0,
+  stagger: 0.05,
+  delay: 0.5,
+  duration: 0.5
+})
+
