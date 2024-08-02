aa5a6a4daa49536af747191a82b1d98774117d50
Mahmoud S Albohaisi <mohabohaisi@gmail.com>
2024-08-02 11:58:08 +0300

    some changes

--- js/main.js
+++ js/main.js
@@ -3,23 +3,25 @@
 const openBarBtn = document.querySelector(".bars");
 const closeBarBtn = document.querySelector(".close");
 const listLink = document.querySelector("ul.links");
+const header = document.querySelector("header");
 
-openBarBtn.addEventListener("click", function () {
-  listLink.classList.add("active");
-});
-
-closeBarBtn.addEventListener("click", function () {
+const removeAcitve = function () {
   listLink.classList.remove("active");
-});
+};
+const addAcitve = function () {
+  listLink.classList.add("active");
+};
+
+openBarBtn.addEventListener("click", addAcitve);
+closeBarBtn.addEventListener("click", removeAcitve);
 
 // GSAP CODE
 
 const myText = new SplitType("#reveal");
 console.log(myText);
-gsap.to('.char', {
+gsap.to(".char", {
   y: 0,
   stagger: 0.05,
   delay: 0.5,
-  duration: 0.5
-})
-
+  duration: 0.5,
+});
